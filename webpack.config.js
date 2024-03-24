4e26fde9d1bb117b27a34f7a82b922dbaba44b28
Malay Mishra <malaymishra@Malays-Air.lan>
2024-03-24 17:40:17 +0530

    addded comments

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,29 @@ const singleSpaDefaults = require("webpack-config-single-spa-react");
 const path = require("path");
 
 module.exports = (webpackConfigEnv) => {
-  const defaultConfig = singleSpaDefaults({
+  const defaultConfig = singleSpaDefaults({ // Tailor the output including buildentry and output point on basis of props
     orgName: "mf-demo",
     projectName: "navbar",
     webpackConfigEnv,
   });
+
+  // entry: '/Users/malaymishra/Downloads/ALL_TECH/MicroFrontEnd/navbar/src/mf-demo-navbar.js',
+  // output: {
+  //   filename: 'mf-demo-navbar.js',
+  //   libraryTarget: 'system',
+  //   path: '/Users/malaymishra/Downloads/ALL_TECH/MicroFrontEnd/navbar/dist',
+  //   jsonpFunction: 'webpackJsonp_navbar',
+  //   devtoolNamespace: 'navbar'
+  // },
+  // module: { rules: [ [Object], [Object], [Object] ] },
+  // devtool: 'sourcemap',
+  // devServer: {
+  //   compress: true,
+  //   historyApiFallback: true,
+  //   headers: { 'Access-Control-Allow-Origin': '*' },
+  //   disableHostCheck: true
+  // },
+  // externals: [ 'single-spa', /^@mf-demo\//, 'react', 'react-dom' ],
 
   return webpackMerge.smart(defaultConfig, {
     // customizations can go here
